c3dd6e142a195beb76db487afca84fe4ad20fd28
mqnoy <qnoy.rifky@gmail.com>
2021-03-07 18:45:13 +0700

    add serial receiver class

--- receiver/serialreceiver.cpp
+++ receiver/serialreceiver.cpp
@@ -1,6 +1,47 @@
 #include "serialreceiver.h"
 
-SerialReceiver::SerialReceiver(QObject *parent) : QObject(parent)
+SerialReceiver::SerialReceiver(QObject *parent) : QThread(parent)
+{}
+
+void SerialReceiver::run()
+{
+    while(1){
+        //emit readSerial();
+    }
+}
+
+void SerialReceiver::init(QString comPort, qint32 boudRate)
 {
 
+    serialport = new QSerialPort;
+    serialport->setPortName(comPort);
+    serialport->setBaudRate(boudRate);
+    serialport->setDataBits(QSerialPort::Data8);
+    serialport->setParity(QSerialPort::NoParity);
+    serialport->setStopBits(QSerialPort::OneStop);
+    serialport->setFlowControl(QSerialPort::NoFlowControl);
+
+    if (!serialport->open(QSerialPort::ReadWrite)) {
+        qDebug() << "Error opening serial port: " << serialport->errorString();
+    }else{
+        qDebug() << "Success opening serial port: " << serialport->errorString();
+        connect(serialport, &QSerialPort::readyRead,
+                this, &SerialReceiver::readSerial);
+        serialport->write("Halo from me another side..\n");
+    }
+}
+
+void SerialReceiver::readSerial()
+{
+    QByteArray buff;
+    buff.append(serialport->readAll());
+
+    qDebug() << QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss:zzz")<<" Receiving from: "<<serialport->portName()<<" buff: "<<buff;
+    buff.clear();
+}
+
+void SerialReceiver::writeSerial(QString comPort, QByteArray payloads)
+{
+    qDebug() <<"writeSerial: "<<comPort <<"payloads : "<<payloads;
+    serialport->write(payloads, payloads.length());
 }
--- receiver/serialreceiver.h
+++ receiver/serialreceiver.h
@@ -1,15 +1,34 @@
 #ifndef SERIALRECEIVER_H
 #define SERIALRECEIVER_H
 
+#include <QSerialPort>
 #include <QObject>
+#include <QDebug>
+#include <QThread>
+#include <QDateTime>
 
-class SerialReceiver : public QObject
+class SerialReceiver : public QThread
 {
     Q_OBJECT
 public:
     explicit SerialReceiver(QObject *parent = nullptr);
-
 signals:
+
+public slots:
+    // init
+    void init(QString comPort, qint32 boudRate);
+
+    // read serial
+    void readSerial();
+
+    // write serial
+    void writeSerial(QString comPort, QByteArray payloads);
+
+protected:
+    void run() override;
+
+private:
+    QSerialPort *serialport;
 
 };
 
